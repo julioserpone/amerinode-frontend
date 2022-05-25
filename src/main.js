1fd2d573c84eb3d89c96142765e6978a560f44ed
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-05-25 17:28:25 -0500

    Install primevue

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './tailwind.css'
 import App from './App.vue'
 import router from './routes.js'
 import Notifications from 'notiwind'
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 app
     .use(router)
     .use(Notifications)
+    .use(PrimeVue, { ripple: true })
     .mount('#app')
