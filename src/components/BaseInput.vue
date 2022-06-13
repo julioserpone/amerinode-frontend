156167ed913b4b555a019a4eda5130bb0fcf0a30
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-06-13 16:00:34 -0500

    feat: Base Input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <label class="block text-sm font-medium text-gray-700">{{ label }}</label>
+  <input
+      v-bind="$attrs"
+      :id="id"
+      :name="id"
+      :autocomplete="autocomplete"
+      :placeholder="label"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="mt-1 focus:ring-amerinode-blue-500 focus:border-amerinode-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+</template>
+<script setup>
+  defineProps({
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    autocomplete: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  })
+</script>
