a7b3f26a0c3e8c321cedcb9fdef6c0ca518906ab
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-06-21 15:11:43 -0500

    feat: Disabled attribute for BaseInput.vue component

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -2,13 +2,16 @@
   <label class="block text-sm font-medium text-gray-700">{{ label }}</label>
   <input
       v-bind="$attrs"
+      v-bind:disabled="isDisabled"
       :id="id"
       :name="id"
       :autocomplete="autocomplete"
       :placeholder="label"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="mt-1 focus:ring-amerinode-blue-500 focus:border-amerinode-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+      class="mt-1 focus:ring-amerinode-blue-500 focus:border-amerinode-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+      :class="[ isDisabled ? 'disabled:opacity-75':'']"
+  />
 </template>
 <script setup>
   defineProps({
@@ -27,6 +30,10 @@
     modelValue: {
       type: [String, Number],
       default: ''
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
     }
   })
 </script>
