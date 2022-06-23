b77099a06654f5d6c13329bf1825d339f4d5b14f
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-06-23 10:17:11 -0500

    refactor: add v-bind attrs to loading button component

--- src/components/LoadingButton.vue
+++ src/components/LoadingButton.vue
@@ -20,8 +20,12 @@
 </script>
 
 <template>
-  <button :type="typeButton" class="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-opacity-90 relative"
-    :class="[ backgroundColor, 'focus:'+focusColor, isLoading ? 'disabled:opacity-75':'']" :disabled="isLoading"
+  <button
+      v-bind="$attrs"
+      :type="typeButton"
+      class="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-opacity-90 relative"
+      :class="[ backgroundColor, 'focus:'+focusColor, isLoading ? 'disabled:opacity-75':'']"
+      :disabled="isLoading"
   >
     <svg v-show="isLoading" class="w-5 h-5 mr-2 text-white animate-spin" fill="none"
          viewBox="0 0 24 24"
