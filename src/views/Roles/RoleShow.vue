8425b9b35e475f80e7800ec2b39419409393516f
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-06-24 15:16:53 -0500

    feat: form to show role

---
+++ src/views/Roles/RoleShow.vue
@@ -0,0 +1,68 @@
+<template>
+  <main>
+    <loading-content :is-loading="loading" />
+    <div v-show="!loading" class="px-4 py-4 sm:px-6 lg:px-8">
+      <div class="mx-auto">
+        <breadcrumbs :trace-route="trace" />
+        <div class="pb-5 border-b border-gray-200">
+          <h1 class="text-2xl font-semibold text-gray-900 pt-4">Role {{ roleName }}</h1>
+        </div>
+      </div>
+      <div class="mx-auto">
+        <RoleForm :role="roleData" :role-id="roleId" :assign-list="canAssign" :can-edit="canEdit" @isLoading="statusLoading" />
+      </div>
+    </div>
+  </main>
+</template>
+<script setup>
+import { onBeforeMount, ref } from "vue";
+import { useRouter, useRoute } from 'vue-router'
+import roleService from "@/services/rol.service";
+import Breadcrumbs from '@/components/Breadcrumbs.vue'
+import LoadingContent from '@/components/LoadingContent.vue'
+import RoleForm from '@/views/Roles/RoleForm.vue'
+
+const route = useRoute()
+const router = useRouter()
+const canEdit = ref(false)
+const canAssign = ref(true)
+let roleData = ref({
+  "name": "",
+  "description": "",
+  "guard_name": "",
+  "roleId": "",
+  "status": "",
+  "permissions": [{
+    "id": 0,
+    "name": "",
+    "guard_name": "",
+    "description": "",
+  }]
+})
+
+let loading = ref(true)
+let roleId = ref('')
+let roleName = ref(String)
+
+//for breadcrumbs
+let trace = [
+  { description: 'Home', pathName: 'HomePage', isLink: true, current: false },
+  { description: 'Roles', pathName: 'RoleList', isLink: true, current: false },
+  { description: 'Show', pathName: 'RoleShow', isLink: false, current: true }
+]
+onBeforeMount( () => {
+
+  roleId.value = route.params.roleId
+
+  roleService.edit(roleId.value).then(x => {
+    roleData.value = x.data
+    roleName.value = x.data.name
+  }).catch(err => {
+  }).finally(() => {
+  })
+})
+
+const statusLoading = (data) => {
+  loading.value = data
+}
+</script>
