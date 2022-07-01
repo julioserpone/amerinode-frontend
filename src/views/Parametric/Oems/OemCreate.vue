3341876bde8b876e0eb299e72f63eef82e794dc5
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-01 15:06:34 -0500

    feat: OEM create form

---
+++ src/views/Parametric/Oems/OemCreate.vue
@@ -0,0 +1,74 @@
+<template>
+  <main>
+    <loading-content :is-loading="loading" />
+    <div v-show="!loading" class="px-4 py-4 sm:px-6 lg:px-8">
+      <div class="mx-auto">
+        <breadcrumbs :trace-route="trace" />
+        <div class="pb-5 border-b border-gray-200">
+          <h1 class="text-2xl font-semibold text-gray-900 pt-4">New OEM</h1>
+        </div>
+      </div>
+      <div class="mx-auto">
+        <OemForm
+            :oem="oemData"
+            :oem-id="oemId"
+            :assign-list="canAssign"
+            :mode-edit="false"
+            @isLoading="statusLoading"
+            @save="saveData" />
+      </div>
+    </div>
+  </main>
+</template>
+<script setup>
+import { ref } from "vue";
+import Breadcrumbs from '@/components/Breadcrumbs.vue'
+import LoadingContent from '@/components/LoadingContent.vue'
+import OemForm from '@/views/Parametric/Oems/OemForm.vue'
+import oemService from "@/services/oem.service"
+import {notify} from "notiwind"
+import {useRouter} from "vue-router"
+
+const router = useRouter()
+const canAssign = ref(false)
+let loading = ref(false)
+let oemId = ref('')
+let oemData = ref({
+  "description": "",
+  "status": ""
+})
+
+let trace = [
+  { description: 'Home', pathName: 'HomePage', isLink: true, current: false },
+  { description: 'Parametric', pathName: 'ParametricPage', isLink: true, current: false },
+  { description: 'OEMs', pathName: 'OemList', isLink: true, current: false },
+  { description: 'Create', pathName: 'OemCreate', isLink: false, current: true }
+]
+const saveData = (event) => {
+  let data = {
+    'oem': event.oem,
+    'status': event.status
+  }
+  oemService.save(0, data).then(x => {
+    notify({
+      group: "top",
+      title: "Update",
+      text: x.data,
+      type: "success"
+    }, 5000)
+    router.push({ name: "OemList" })
+  }).catch(err => {
+    let message = (err.code === "ERR_BAD_REQUEST") ? err.response.data.message : err.message;
+    notify({
+      group: "top",
+      title: "Error",
+      text: message,
+      type: "error"
+    }, 5000)
+  }).finally(() => {
+  })
+}
+const statusLoading = (data) => {
+  loading.value = data
+}
+</script>
