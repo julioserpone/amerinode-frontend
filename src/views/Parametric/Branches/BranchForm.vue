251068fa2e79f8bdb984e0a5dce02da4724d3c9a
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-07 17:48:24 -0500

    feat: branch edit view

--- src/views/Parametric/Branches/BranchForm.vue
+++ src/views/Parametric/Branches/BranchForm.vue
@@ -5,24 +5,87 @@
         <div class="md:col-span-1">
           <div class="px-4 sm:px-0">
             <h3 class="text-lg font-medium leading-6 text-gray-900">Information</h3>
-            <p class="mt-1 text-sm text-gray-600">Descriptive information of the company.</p>
+            <p class="mt-1 text-sm text-gray-600">Relationship of the company with the country where the branch is located.</p>
           </div>
         </div>
         <div class="mt-5 md:mt-0 md:col-span-2">
           <form action="#" method="POST">
-            <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="shadow sm:rounded-md">
               <div class="px-4 py-5 bg-white sm:p-6">
                 <div class="grid grid-cols-6 gap-6">
 
-                  <div class="col-span-6 sm:col-span-5">
-                    <base-input
-                        :id="'description-company'"
-                        :autocomplete="'description-company'"
-                        v-model="company.description"
-                        label="Description"
-                        type="text"
-                        :is-disabled="!canEdit"
-                    />
+                  <div class="col-span-6 sm:col-span-5 lg:col-span-5">
+                    <Listbox as="div" v-model="selectedCountry" :disabled="!canEdit">
+                      <ListboxLabel class="block text-sm font-medium text-gray-700"> Country </ListboxLabel>
+                      <div class="relative mt-1">
+                        <ListboxButton class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-amerinode-blue-500 focus:border-amerinode-blue-500 sm:text-sm"
+                                       :class="[ !canEdit ? 'disabled:opacity-75':'']">
+                          <span class="block truncate">{{ selectedCountry.name }}</span>
+                          <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          </span>
+                        </ListboxButton>
+                        <transition
+                            leave-active-class="transition duration-100 ease-in"
+                            leave-from-class="opacity-100"
+                            leave-to-class="opacity-0"
+                        >
+                          <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            <ListboxOption
+                                v-slot="{ active, selected }"
+                                v-for="country in countries"
+                                :key="country.id"
+                                :value="country"
+                                as="template"
+                            >
+                              <li :class="[active ? 'bg-amerinode-blue-100 text-amerinode-blue-900' : 'text-gray-900','relative cursor-default select-none py-2 pl-10 pr-4']">
+                                <span :class="[selected ? 'font-medium' : 'font-normal','block truncate']">{{ country.name }}</span>
+                                <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-amerinode-blue-600">
+                                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              </li>
+                            </ListboxOption>
+                          </ListboxOptions>
+                        </transition>
+                      </div>
+                    </Listbox>
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-5 lg:col-span-5">
+                    <Listbox as="div" v-model="selectedCompany" :disabled="!canEdit">
+                      <ListboxLabel class="block text-sm font-medium text-gray-700"> Company </ListboxLabel>
+                      <div class="relative mt-1">
+                        <ListboxButton class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-amerinode-blue-500 focus:border-amerinode-blue-500 sm:text-sm"
+                                       :class="[ !canEdit ? 'disabled:opacity-75':'']">
+                          <span class="block truncate">{{ selectedCompany.description }}</span>
+                          <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                          </span>
+                        </ListboxButton>
+                        <transition
+                            leave-active-class="transition duration-100 ease-in"
+                            leave-from-class="opacity-100"
+                            leave-to-class="opacity-0"
+                        >
+                          <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            <ListboxOption
+                                v-slot="{ active, selected }"
+                                v-for="company in companies"
+                                :key="company.id"
+                                :value="company"
+                                as="template"
+                            >
+                              <li :class="[active ? 'bg-amerinode-blue-100 text-amerinode-blue-900' : 'text-gray-900','relative cursor-default select-none py-2 pl-10 pr-4']">
+                                <span :class="[selected ? 'font-medium' : 'font-normal','block truncate']">{{ company.description }}</span>
+                                <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-amerinode-blue-600">
+                                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              </li>
+                            </ListboxOption>
+                          </ListboxOptions>
+                        </transition>
+                      </div>
+                    </Listbox>
                   </div>
 
                   <div class="col-span-6 sm:col-span-3 lg:col-span-3">
@@ -84,21 +147,61 @@ import {
   ListboxOption,
 } from '@headlessui/vue'
 import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid'
-import BaseInput from '@/components/BaseInput.vue'
+import countryService from "@/services/country.service";
+import companyService from "@/services/company.service";
 
 let selectedStatus = ref({})
+let selectedCountry = ref({})
+let selectedCompany = ref({})
+let countries = ref()
+let companies = ref()
 let loaded = ref(false)
 const statuses = ref([
   { id: 'active', name: 'Active' },
   { id: 'inactive', name: 'Inactive' }
 ])
 onBeforeMount(() => {
-  if (!data.modeEdit.value) {
+  /*if (!data.modeEdit.value) {
     assignStatus()
     emit('isLoading', false)
-  }
+  }*/
+  companyService.list().then(x => {
+    companies.value = x.data
+    if (data.assignList.value) {
+      //Assign company
+      //Assign the same type of object to the list (for the cases in which it is an already registered company)
+      for (const [index, item] of Object.entries(companies.value)) {
+        if (item.id === data.branch.value.company.id) {
+          selectedCompany.value = companies.value[index]
+        }
+      }
+    } else {
+      selectedCountry.value = countries.value[0]
+    }
+  }).catch(err => {
+  }).finally(() => {
+  })
+
+  countryService.list().then(x => {
+    countries.value = x.data
+    if (data.assignList.value) {
+      //Assign country
+      //Assign the same type of object to the list (for the cases in which it is an already registered country)
+      for (const [index, item] of Object.entries(countries.value)) {
+        if (item.id === data.branch.value.country.id) {
+          selectedCountry.value = countries.value[index]
+        }
+      }
+    } else {
+      selectedCountry.value = countries.value[0]
+    }
+    assignStatus()
+    emit('isLoading', false)
+  }).catch(err => {
+  }).finally(() => {
+  })
 })
-onUpdated( () => {
+/*onUpdated( () => {
   if (data.modeEdit.value) {
     if (!loaded.value) {
       assignStatus()
@@ -106,10 +209,10 @@ onUpdated( () => {
       loaded.value = true
     }
   }
-})
+})*/
 
 const props = defineProps({
-  company: {
+  branch: {
     type: Object
   },
   canEdit: {
@@ -120,7 +223,7 @@ const props = defineProps({
     type: Boolean,
     default: false
   },
-  companyId: {
+  branchId: {
     type: String,
     default: ''
   },
@@ -137,7 +240,7 @@ function assignStatus() {
   if (data.assignList.value) {
     //Assign status
     statuses.value.forEach((element, index) => {
-      if (element.id === data.company.value.status) {
+      if (element.id === data.branch.value.status) {
         selectedStatus.value = element
       }
     })
@@ -147,6 +250,6 @@ function assignStatus() {
 }
 const sendData = (event) => {
   event.preventDefault()
-  emit('save', { company: data.company.value, status: selectedStatus.value } )
+  emit('save', { country: selectedCountry.value, company: selectedCompany.value, status: selectedStatus.value } )
 }
 </script>
