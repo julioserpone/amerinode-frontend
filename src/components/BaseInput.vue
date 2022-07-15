6fff07db70b50a2193d27fe72b8f9c40741aa3d4
Julio Cesar Hernandez <juliohernandezs@gmail.com>
2022-07-15 12:35:21 -0500

    feat: add required attribut to base input component

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -7,6 +7,7 @@
       :name="id"
       :autocomplete="autocomplete"
       :placeholder="label"
+      :required="isRequired"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
       class="mt-1 focus:ring-amerinode-blue-500 focus:border-amerinode-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
@@ -32,6 +33,10 @@
       default: ''
     },
     isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    isRequired: {
       type: Boolean,
       default: false
     }
